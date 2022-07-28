7672b7a37a1714f0878a64f3ad4c0d93912bcf81
nodir <nbotirov2000@gmail.com>
2022-07-28 20:54:44 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/StudentService.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository repository;
+
+    @Autowired
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Student> getStudents() {
+        return repository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = repository
+                .findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("email taken ");
+        }
+        repository.save(student);
+    }
+}
